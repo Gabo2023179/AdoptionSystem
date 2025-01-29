38333296e723c66b4e252e3ea9282c5f8df120d5
Informatica <informatica@alumno.local>
2025-01-29 17:14:40 -0600

    register complete

---
+++ src/helpers/db-validators.js
@@ -0,0 +1,9 @@
+import User from "../user/user.model.js"
+
+export const existeEmail = async(email = '' ) => {
+    const existe = await User.findOne({email})
+    if(existe){
+        throw new Error(`El email ${email} ya fue registrado previamente`)
+
+    }
+}
---
+++ src/user/user.model.js
@@ -0,0 +1,55 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = Schema({
+    name:{
+        type: String,
+        required: [true, "Name is required"],
+        maxLength: [25, "Name cannot exceed 25 characters"]
+    },
+    surname:{
+        type: String,
+        required: [true, "Surname is required"],
+        maxLength: [25, "Surname cannot exceed 25 characters"]
+    },
+    username:{
+        type: String,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength: 8
+
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    profilePicture:{
+        type: String
+    },
+    phone:{
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 8
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ["ADMIN_ROLE", "USER_ROLE"]
+    },
+    status:{
+        type: Boolean,
+        default: true
+    }
+
+},
+    {
+    versionKey: false,
+    timeStamps: true
+    }
+)
+
+export default model("User", userSchema)
